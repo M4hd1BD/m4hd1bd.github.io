8b2b70adf3f90e4de52f0545ac2c417658a570fa
AL Mahdi <virtualmahdi@gmail.com>
2020-06-07 14:17:51 +0600

    backspace button now works as intended

--- JSPros/vkey/scripts/script.js
+++ JSPros/vkey/scripts/script.js
@@ -9,6 +9,16 @@ keys.forEach(item => {
 
 function test(item) {
   var value = item.innerHTML;
-  var textField = document.getElementById('textField');
-  textField.value = textField.value + value;
+  var textFieldID = document.getElementById('textField');
+  var textFieldValue = textFieldID.value;
+  var textFieldLenght = textFieldValue.length;
+
+  if (item.id == 'backpsace') {
+    newTextFieldValue = textFieldValue.replace(textFieldValue[textFieldLenght - 1],'');
+    textFieldID.value = newTextFieldValue;
+  }
+  else {
+    textFieldID.value = textFieldValue + value;
+  }
+
 }
