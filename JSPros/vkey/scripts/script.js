73f1031f1437df6d566566c0c45c717b4a9da9c6
AL Mahdi <virtualmahdi@gmail.com>
2020-06-08 12:54:58 +0600

    added shift button functionality, it now works like any android keyboard

--- JSPros/vkey/scripts/script.js
+++ JSPros/vkey/scripts/script.js
@@ -1,11 +1,12 @@
 var keys = document.querySelectorAll('.key');
+var shiftIsActive = false;
+var shiftIsHard = false;
 
 keys.forEach(item => {
   item.addEventListener('click', function() {
     test(item);
   });
 });
-
 
 function test(item) {
   var value = item.innerHTML;
@@ -17,15 +18,52 @@ function test(item) {
     newTextFieldValue = textFieldValue.substring(0, textFieldLenght - 1);
     textFieldID.value = newTextFieldValue;
   }
+  else if (item.id == 'shift') {
+    if (shiftIsActive && !shiftIsHard) {
+      shiftIsActive = true;
+      shiftIsHard = true;
+      keys.forEach(item => {
+        var keyValue = item.innerHTML;
+        item.innerHTML = keyValue.toUpperCase();
+      });
+    }
+    else if (shiftIsActive && shiftIsHard) {
+      keys.forEach(item => {
+        var keyValue = item.innerHTML;
+        item.innerHTML = keyValue.toLowerCase();
+      });
+      shiftIsActive = false;
+      shiftIsHard = false;
+    }
+    else if (!shiftIsActive && !shiftIsHard) {
+      keys.forEach(item => {
+        var keyValue = item.innerHTML;
+        item.innerHTML = keyValue.toUpperCase();
+      });
+      shiftIsActive = true;
+    }
+  }
   else if (item.id == 'space') {
     textFieldID.value = textFieldValue + ' ';
   }
   else if (item.id == 'enter') {
     textFieldID.value = textFieldValue + "\n";
-    console.log('passed');
   }
   else {
-    textFieldID.value = textFieldValue + value;
+    if (shiftIsActive && shiftIsHard) {
+      textFieldID.value = textFieldValue + value;
+    }
+    else if (shiftIsActive && !shiftIsHard) {
+      textFieldID.value = textFieldValue + value;
+      keys.forEach(item => {
+        var keyValue = item.innerHTML;
+        item.innerHTML = keyValue.toLowerCase();
+      });
+      shiftIsActive = false;
+    }
+    else {
+      textFieldID.value = textFieldValue + value;
+    }
   }
 
 }
