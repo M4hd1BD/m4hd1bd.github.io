7ed0e1737d8f7bbb06227b7e1e1665bef7361b05
AL Mahdi <virtualmahdi@gmail.com>
2020-08-19 13:38:08 +0600

    some changes and added cooldown feature

--- status-bot/index.js
+++ status-bot/index.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 const { prfx, tkn } = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+const cooldowns = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -24,15 +25,42 @@ client.on('message', message => {
 	if (!message.content.startsWith(prfx) || message.author.bot) return;
 
 	const args = message.content.slice(prfx.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 
-	if (!client.commands.has(command)) {
+	if (!client.commands.has(commandName)) {
 		return message.reply('Please make sure you wrote the right command.');
 	}
 
-	try {
-		client.commands.get(command).execute(message, args);
+	const command = client.commands.get(commandName);
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
 	}
+
+	const now = Date.now();
+	// get a collection of triggered commands
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 5) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`slow down boi, wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	else {
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	}
+
+	try {
+		command.execute(message, args);
+	}
+
 	catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
