139ded814dbb627d98bf8f3ae00b7e70e9722f54
AL Mahdi <virtualmahdi@gmail.com>
2020-08-19 12:02:42 +0600

    added three very basic command, made the commands dynamic

---
+++ status-bot/commands/avatar.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'avatar',
+	description: 'Check out someones avatar.',
+	execute(message, args) {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format:'png', dynamic: true })}>`);
+		}
+
+		const taggedUser = message.mentions.users.first();
+		return message.channel.send(`${taggedUser}'s avatar: <${taggedUser.displayAvatarURL({ format:'png', dynamic: true })}>`);
+	},
+};
--- status-bot/index.js
+++ status-bot/index.js
@@ -1,6 +1,18 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prfx, tkn } = require('./config.json');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -8,9 +20,24 @@ client.once('ready', () => {
 
 
 client.on('message', message => {
-	if (message.content.startsWith(`${prfx}ping`)) {
-		message.channel.send('I\'m up, I\'m up!');
+
+	if (!message.content.startsWith(prfx) || message.author.bot) return;
+
+	const args = message.content.slice(prfx.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) {
+		return message.reply('Please make sure you wrote the right command.');
 	}
+
+	try {
+		client.commands.get(command).execute(message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+
 });
 
 client.login(tkn);
