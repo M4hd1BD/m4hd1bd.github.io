6b76b49aeea0729ca409086d75687a2117c879c3
AL Mahdi <virtualmahdi@gmail.com>
2020-10-04 11:43:32 +0600

    Added currently online user list

--- nodeJS/lets-chat/index.js
+++ nodeJS/lets-chat/index.js
@@ -18,3 +18,20 @@ const io = socket(server);
 io.on("connection", function (socket) {
   console.log("Made socket connection");
 });
+
+const activeUsers = new Set();
+
+io.on("connection", function (socket) {
+  console.log("Made socket connection");
+
+  socket.on("new user", function (data) {
+    socket.userId = data;
+    activeUsers.add(data);
+    io.emit("new user", [...activeUsers]);
+  });
+
+  socket.on("disconnect", () => {
+    activeUsers.delete(socket.userId);
+    io.emit("user disconnected", socket.userId);
+  });
+});
