d26670ca6d376c351b698487f7b7c2058be60451
AL Mahdi <virtualmahdi@gmail.com>
2020-11-04 20:07:47 +0600

    Revert "added cuteCat command to get Cat pic using TheCatAPI because why not"
    This reverts commit e4002d9f3cf703abb2f338673d72bb724c87bec1.
    
    mistakenly commited the wrong file, reverting

--- status-bot/commands/reload.js
+++ status-bot/commands/reload.js
@@ -5,7 +5,7 @@ module.exports = {
 	execute(message, args) {
 
 		if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
-		const commandName = args[0];
+		const commandName = args[0].toLowerCase();
 		const command = message.client.commands.get(commandName)
 			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
